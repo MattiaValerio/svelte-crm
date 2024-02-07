bb9c14b006e0661f7896d0d26f4d50539b073e19
MattiaValerio <mattia.valerio2002@gmail.com>
2024-02-07 23:00:50 +0100

    Aggiunto svelte-hamburgers e modificato script dev

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,27 @@
-<script>
+<script lang="ts">
+	import Hamburger from './../../node_modules/svelte-hamburgers/dist/Hamburger.svelte';
 	import '../app.css';
+	import Menu from '../components/Menu.svelte';
+
+	let open: boolean;
 </script>
 
+	<div class="bg-black w-screen flex items-center gap-5">
+		<div class="h-full px-3 font-bold flex items-center">
+			<h1 class="text-white text-2xl text-center">MV</h1>
+		</div>
+		<div class=" w-full flex justify-end" >
+			<Hamburger bind:open --color="white"/>
+		</div>
+	</div>
+	<div class="flex flex-col justify-center items-center bg-black pb-2">
+		<Menu bind:open />
+	</div>
+
+
+
+
 <slot />
+
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,7 @@
-<div class="flex flex-col p-5">
-	<form
-		method="post"
-		action="?/addCompany"
-		class="flex flex-col border rounded-md border-black p-3 gap-4"
-	>
-		<div class="flex flex-col">
-			<label for="name">Company name:</label>
-			<input type="text" name="name" id="name" class="border rounded-md h-10 px-3" />
-		</div>
-		<div class="flex flex-col">
-			<label for="description">Company description:</label>
-			<textarea name="description" id="description" class="border rounded-md h-72 px-3 py-2" />
-		</div>
-		<input
-			type="submit"
-			value="Submit"
-			class="bg-blue-400 rounded-md h-14 text-white font-bold hover:bg-blue-500"
-		/>
-	</form>
-</div>
+<script lang="ts">
+	import NewCompany from "../components/NewCompany.svelte";
+
+</script>
+
+
+<NewCompany />
