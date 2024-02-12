511700e8398c9df644ee0737d9a89a82aa57b768
Mattia <mattia.valerio2002@gmail.com>
2024-02-12 17:26:42 +0100

    Remove unused dependencies and files

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,26 +3,12 @@
 declare global {
 	namespace App {
 		// interface Error {}
-		interface Locals {
-			validate: import('@lucia-auth/svelte-kit').Validate;
-			validateuser: import('@lucia-auth/svelte-kit').ValidateUser;
-			setSession: import('@lucia-auth/svelte-kit').SetSession;
-		}
+		//interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
 	}
 	let prisma: PrismaClient;
-
-	declare namespace Lucia {
-		type Auth = import('$lib/server/lucia').Auth;
-		type UserAttributes = {
-			name: string;
-			lastname: string;
-			email: string;
-			roleId: number;
-		};
-	}
 }
 
 export {};
