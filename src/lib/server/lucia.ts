d65735527caf2215559721a6a808ab278829f319
Mattia <mattia.valerio2002@gmail.com>
2024-02-12 17:57:25 +0100

    lucia v3 setup

---
+++ src/lib/server/lucia.ts
@@ -0,0 +1,23 @@
+import { Lucia } from 'lucia';
+import { dev } from '$app/environment';
+import { PrismaAdapter } from '@lucia-auth/adapter-prisma';
+import { PrismaClient } from '@prisma/client';
+
+const client = new PrismaClient();
+
+const adapter = new PrismaAdapter(client.session, client.user);
+
+export const lucia = new Lucia(adapter, {
+	sessionCookie: {
+		attributes: {
+			// set to `true` when using HTTPS
+			secure: !dev
+		}
+	}
+});
+
+declare module 'lucia' {
+	interface Register {
+		Lucia: typeof lucia;
+	}
+}
