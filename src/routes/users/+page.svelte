f48eefaf35290e939cb479645256d26f5d43a969
Mattia <mattia.valerio2002@gmail.com>
2024-03-04 11:49:26 +0100

    edit commited

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -1,36 +1,18 @@
 <script lang="ts">
-	import UserCard from '../../components/UserCard.svelte';
+	import UserCard from './../../components/UserCard.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
 
+
 <!-- create a table -->
-<div class="w-full">
-	<table class="table-auto overflow-x-scroll max-w-full w-full">
-		<thead>
-		  <tr>
-			<th>Nome</th>
-			<th>Cognome</th>
-			<th>Email</th>
-			<th>Role</th>
-		  </tr>
-		</thead>
-		<tbody>
-			{#each data.utenti as user}
-			<tr>
-				<td>{user.name}</td>
-				<td>{user.lastname}</td>
-				<td>{user.email}</td>
-				<td>{user.roles}</td>
-			</tr>
-		{/each}
-		</tbody>
-	  </table>
+<div class="w-full h-full overflow">
+	{#each data.utenti as user}
+		<UserCard {user} />
+	{/each}
 </div>
-	
-	
-	
-	
-	
-	
+
+
+
+
